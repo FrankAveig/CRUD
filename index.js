3c587970a884a6f22098fa965c20f830f0c24829
Frank Aveiga <68234079+FrankAveig@users.noreply.github.com>
2022-06-24 02:39:30 -0500

    temrinada funcion agregar y iconos de editar y eliminar

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-const formulario = document.getElementById('formulario')
+const formulario = document.getElementById('formulario');
+const nombre = document.getElementById('inputNombre');
+const fechaNacimiento = document.getElementById('inputFechaNacimiento');
+const vacuna = document.getElementById('inputVacuna');
+const fechaVacunacion = document.getElementById('inputFechaVacunacion');
+const fechaRevacunacion = document.getElementById('inputFechaRevacunacion');
+const bodyTable = document.getElementById('bodyTable')
+const mascotas = [
+    {
+        nombre:'asdf',
+        fechaNacimiento:'asdf',
+        vacuna:'1',
+        fechaNacimiento:'2',
+        fechaVacunacion:'3',
+        fechaRevacunacion:'3'
+    }
+]
 
 function show(){
     if(formulario.classList.contains('hide')){
@@ -12,3 +28,58 @@ function hide(){
         formulario.classList.add('hide')
     }
 }
+
+function addMascota (nombre, fechaNacimiento,vacuna,fechaVacunacion,fechaRevacunacion){
+    mascotas.push({
+        nombre,
+        fechaNacimiento,
+        vacuna,
+        fechaNacimiento,
+        fechaVacunacion,
+        fechaRevacunacion
+    })
+    mostrarMascotas();
+}
+
+function editMascota(indice){
+    mascotas[indice].nombre
+}
+function limpiar(){
+    nombre.value='';
+    fechaNacimiento.value=''; 
+    vacuna.value='';
+    fechaVacunacion.value='';
+    fechaRevacunacion.value='';
+}
+
+function nuevoIngreso(){
+    if(nombre.value.trim() !== ''&& fechaNacimiento.value.trim()!=='' && vacuna.value.trim() !== '' && fechaVacunacion!== '' && fechaRevacunacion !==''){
+        addMascota(nombre.value,fechaNacimiento.value,vacuna.value,fechaVacunacion.value,fechaRevacunacion.value)
+        limpiar();
+        hide();
+    }else{
+        alert("Debe llenar todos los campos");
+    }
+
+}
+
+
+
+function mostrarMascotas(){
+    bodyTable.innerHTML= '';
+    mascotas.forEach(function(mascota,indice) {
+        bodyTable.innerHTML += `
+                <td>${indice +1}</td>
+                <td>${mascota.nombre}</td>
+                <td>${mascota.fechaNacimiento}</td>
+                <td>${mascota.vacuna}</td>
+                <td>${mascota.fechaVacunacion}</td>
+                <td>${mascota.fechaRevacunacion}</td>
+                <td><i  data-bs-toggle="modal" data-bs-target="#editModal" class="fas fa-edit"></i>
+                <td><i class="fas fa-backspace"></i>
+        
+        `
+    })
+}
+
+mostrarMascotas();
